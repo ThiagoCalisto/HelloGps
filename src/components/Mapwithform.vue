27c1507f6a8665a3e68b4112461821fff3d8115c
ThiagoCalisto <warlocalgames@gmail.com>
2023-11-03 18:08:25 -0300

    feat: adicao front-end e medir distancia

--- src/components/Mapwithform.vue
+++ src/components/Mapwithform.vue
@@ -1,5 +1,6 @@
 <!-- MapWithForm.vue -->
 <template>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <div class="container">
         <form @submit.prevent="submitForm">
             <div class="group-one">
@@ -40,7 +41,6 @@
                         <option value="DF">Distrito Federal</option>
                         <option value="GO">Goiás</option>
                         <option value="MG">Minas Gerais</option>
-                        <!-- Adicione mais opções conforme necessário -->
                     </select>
                 </div>
                 <div class="form-group-secondary">
@@ -49,13 +49,35 @@
                         <option value="Brasília">Brasília</option>
                         <option value="Goiânia">Goiânia</option>
                         <option value="Belo Horizonte">Belo Horizonte</option>
-                        <!-- Adicione mais opções conforme necessário -->
                     </select>
                 </div>
             </div>
         </form>
 
-        <div ref="map" class="map"></div>
+        <div class="map-container">
+            <div ref="map" class="map">
+            </div>
+            <div class="map-buttons">
+                <button @click="addMarkerOnClick" type="submit">
+                    <img src="../../img/pointer.png" style="width:23px; height:23px;">
+                </button>
+                <button @click="startDrawing" type="submit">
+                    <img src="../../img/regua-triangular.png" style="width:23px; height:23px;">
+                </button>
+                <button @click="measureDistance" type="submit">
+                    <img src="../../img/governante.png" style="width:23px; height:23px;">
+                </button>
+                <button @click="clearDraw" type="submit">
+                    <img src="../../img/borracha.png" style="width:23px; height:23px;">
+                </button>
+                <button @click="buscarRua" type="submit">
+                    <img src="../../img/mapa.png" style="width:23px; height:23px;">
+                </button>
+                <button @click="clearMarker" type="submit">
+                    <img src="../../img/lixo.png" style="width:23px; height:23px;">
+                </button>
+            </div>
+        </div>
     </div>
 </template>
   
@@ -70,19 +92,23 @@ import { Style, Icon } from 'ol/style';
 import { Vector as VectorSource } from 'ol/source';
 import VectorLayer from 'ol/layer/Vector';
 import Draw from 'ol/interaction/Draw.js';
+import { Text } from 'vue';
+import { LineString } from 'ol/geom';
+import { getLength } from 'ol/sphere';
+
 
 
 export default {
-    name: 'MapWithForm',
+    name: "MapWithForm",
     data() {
         return {
-            cep: '',
-            endereco: '',
-            numero: '',
-            bairro: '',
-            complemento: '',
-            estado: '',
-            municipio: '',
+            cep: "",
+            endereco: "",
+            numero: "",
+            bairro: "",
+            complemento: "",
+            estado: "",
+            municipio: "",
             isAddMarkerButtonClicked: false,
             map: null,
             draw: null,
@@ -107,43 +133,40 @@ export default {
             const raster = new TileLayer({
                 source: new OSM(),
             });
-            this.map = new Map({
-                target: this.$refs.map,
-                layers: [raster, se,
-                    new TileLayer({
-                        source: new OSM(),
-                    }),
-                ],
-                view: new View({
-                    center: [-5429488.227348947, -3198201.0568349618],
-                    zoom: 8,
-                }),
+
+            const projection = new View({
+                center: [-48.5480, -27.5954],
+                zoom: 10,
+                projection: 'EPSG:4326',
             });
 
-            this.map.on('click', this.handleMapClick)
+            this.map = new Map({
+                target: this.$refs.map,
+                layers: [raster, se, new TileLayer({
+                    source: new OSM(),
+                }),
+                ],
+                view: projection,
+            });
+
+            this.map.on("click", this.handleMapClick);
         },
 
         addMarkerOnClick() {
             this.isAddMarkerButtonClicked = true;
         },
-
         clearDraw() {
             if (this.draw) {
                 this.map.removeInteraction(this.draw);
                 this.vectorLayer.getSource().clear();
                 this.map.removeLayer(this.vectorLayer);
                 this.draw = null;
-
-
                 this.uniqueFeatures.forEach(feature => {
                     this.vectorLayer.getSource().removeFeature(feature);
                 });
-
-
                 this.uniqueFeatures.clear();
             }
         },
-
         clearMarker() {
             if (this.markerLayer) {
                 this.map.removeLayer(this.markerLayer);
@@ -151,140 +174,133 @@ export default {
                 this.markerFeature = null;
             }
         },
-
-
         startDrawing() {
             if (this.draw) {
                 this.map.removeInteraction(this.draw);
             }
-
             const source = new VectorSource({ wrapX: false });
-
             this.draw = new Draw({
                 source: source,
-                type: 'Polygon',
+                type: "Polygon",
             });
-
-            this.draw.on('drawend', (event) => {
+            this.draw.on("drawend", (event) => {
                 const feature = event.feature;
-
                 if (!this.uniqueFeatures.has(feature)) {
                     this.vectorLayer.getSource().addFeature(feature);
                     this.uniqueFeatures.add(feature);
                 }
             });
-
             this.map.addLayer(this.vectorLayer);
             this.map.addInteraction(this.draw);
         },
-
-
-
         handleMapClick(event) {
             const coordinates = event.coordinate;
-            console.log('Clicked at Coordinates:', coordinates);
+            console.log("Clicked longitude:", coordinates[0]);
+            console.log("Clicked latitude:", coordinates[1]);
             this.addMarker(coordinates);
         },
-
         addMarker(coordinates) {
             if (this.isAddMarkerButtonClicked) {
                 if (this.markerFeature) {
                     this.markerFeature.getGeometry().setCoordinates(coordinates);
-                } else {
+                }
+                else {
                     this.markerFeature = new Feature({
                         geometry: new Point(coordinates),
                     });
-
                     const markerStyle = new Style({
                         image: new Icon({
                             anchor: [0.5, 1],
-                            src: '../../img/icon.png',
+                            src: "../../img/placeholder.png",
+                            scale: 0.1,
                         }),
                     });
-
                     this.markerFeature.setStyle(markerStyle);
-
                     this.markerLayer = new VectorLayer({
                         source: new VectorSource({
                             features: [this.markerFeature],
                         }),
                     });
-
                     this.map.addLayer(this.markerLayer);
                 }
                 this.isAddMarkerButtonClicked = false;
             }
         },
-
         measureDistance() {
             if (this.draw) {
                 this.map.removeInteraction(this.draw);
             }
 
             const source = new VectorSource({ wrapX: false });
-
             this.draw = new Draw({
                 source: source,
-                type: 'LineString',
+                type: "LineString",
             });
 
-            this.draw.on('drawend', (event) => {
+            this.draw.on("drawend", (event) => {
                 const feature = event.feature;
                 this.vectorLayer.getSource().addFeature(feature);
+
                 const geometry = feature.getGeometry();
                 const coordinates = geometry.getCoordinates();
-                const length = Math.round(geometry.getLength() * 100) / 100;
-                console.log(`Distância medida: ${length} metros`);
+
+                
+                const transformedCoordinates = coordinates.map(coord =>
+                    fromLonLat(coord, 'EPSG:4326')
+                );
+
+                const lineString = new LineString(transformedCoordinates);
+    
+                const wgs84Length = getLength(lineString, { projection: 'EPSG:4326' });
+
+                const formattedLength = wgs84Length.toFixed(2);
+
+                console.log(`Distância medida: ${formattedLength} metros no sistema WGS84`);
             });
+
             this.map.addLayer(this.vectorLayer);
             this.map.addInteraction(this.draw);
         },
 
         async buscarRua() {
             try {
-                const resposta = await fetch(`https://nominatim.openstreetmap.org/search?q=${this.endereco}&format=json`);
-                const dados = await resposta.json();
+                const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${this.endereco}&format=json`);
+                const dados = await response.json();
+                console.log(dados);
 
                 if (dados && dados.length > 0) {
                     const firstResult = dados[0];
                     const latitude = parseFloat(firstResult.lat);
                     const longitude = parseFloat(firstResult.lon);
 
-                    const coordinates = fromLonLat([longitude, latitude]);
+                    // Ajuste para coordenadas no formato EPSG:4326 (WGS84)
+                    const coordinates = [longitude, latitude];
+
                     this.map.getView().setCenter(coordinates);
                     this.map.getView().setZoom(17);
-
-
                     this.clearDraw();
-
                     this.clearMarker();
                     this.addMarker(coordinates);
-
                 } else {
-                    console.error('Endereço não encontrado.');
+                    console.error("Endereço não encontrado.");
                 }
             } catch (erro) {
                 console.error(erro);
             }
         },
-
-
-
         async submitForm() {
             try {
                 if (this.cep) {
                     const response = await fetch(`https://viacep.com.br/ws/${encodeURIComponent(this.cep)}/json/`);
                     const data = await response.json();
-
                     if (!data.erro) {
-                        this.endereco = data.logradouro || '';
-                        this.numero = data.numero || '';
-                        this.bairro = data.bairro || '';
-                        this.complemento = data.complemento || '';
-                        this.estado = data.uf || '';
-                        this.municipio = data.localidade || '';
-
-                        console.log('Formulário enviado:', {
+                        this.endereco = data.logradouro || "";
+                        this.numero = data.numero || "";
+                        this.bairro = data.bairro || "";
+                        this.complemento = data.complemento || "";
+                        this.estado = data.uf || "";
+                        this.municipio = data.localidade || "";
+                        console.log("Formulário enviado:", {
                             cep: this.cep,
                             endereco: this.endereco,
                             numero: this.numero,
@@ -293,46 +309,43 @@ export default {
                             estado: this.estado,
                             municipio: this.municipio,
                         });
-
                         this.updateMap();
-                    } else {
-                        console.error('CEP não encontrado.');
                     }
-                } else {
-                    console.error('Campo "CEP" é obrigatório.');
+                    else {
+                        console.error("CEP não encontrado.");
+                    }
                 }
-            } catch (error) {
-                console.error('Erro ao buscar localização:', error.message);
+                else {
+                    console.error("Campo \"CEP\" é obrigatório.");
+                }
+            }
+            catch (error) {
+                console.error("Erro ao buscar localização:", error.message);
             }
         },
         async updateMap() {
             try {
-                const response = await fetch(`https://brasilapi.com.br/api/cep/v2/${this.cep.replace("/-|\./gi, ''")}`);
+                const response = await fetch(`https://brasilapi.com.br/api/cep/v2/${this.cep.replace(/-|\./gi, '')}`);
                 const data = await response.json();
+
                 console.log(data);
 
                 if (data) {
                     const result = data;
-                    const coordinates = fromLonLat([parseFloat(result.location.coordinates.longitude), parseFloat(result.location.coordinates.latitude)]);
+
+                    const coordinates = [parseFloat(result.location.coordinates.longitude), parseFloat(result.location.coordinates.latitude)];
+
                     this.map.getView().setCenter(coordinates);
                     this.map.getView().setZoom(10);
-
                     const [longitude, latitude] = coordinates.map(coord => coord.toFixed(6));
-
                     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
                 } else {
-                    console.error('Localização não encontrada para o CEP fornecido.');
+                    console.error("Localização não encontrada para o CEP fornecido.");
                 }
             } catch (error) {
-                console.error('Erro ao buscar localização:', error.message);
+                console.error("Erro ao buscar localização:", error.message);
             }
         },
-
-
-
-
-
-
     },
     watch: {
         vectorLayerFeatures: {
@@ -343,6 +356,7 @@ export default {
             deep: true,
         },
     },
+    components: { Text }
 };
 </script>
   
@@ -359,7 +373,6 @@ export default {
 .group-one {
     display: flex;
     flex-wrap: wrap;
-    /* Permite que os elementos quebrem para a próxima linha quando não couberem na largura */
     margin-bottom: 10px;
     gap: 20px;
 }
@@ -367,15 +380,12 @@ export default {
 .form-group label {
     flex: 1;
     min-width: 100px;
-    /* Largura mínima para o rótulo (ajuste conforme necessário) */
     text-align: right;
-    /* Alinha o rótulo à direita */
 }
 
 .form-group input {
     flex: 2;
     width: 100%;
-    /* Ocupa 100% da largura disponível */
     height: 30px;
 }
 
@@ -407,18 +417,29 @@ export default {
 
 .form-group-secondary input {
     width: 100%;
-    /* Ocupa 100% da largura disponível */
     height: 30px;
 }
+
 .form-group-secondary select {
     width: 100%;
-    /* Ocupa 100% da largura disponível */
     height: 30px;
 }
 
 .map {
     width: 100%;
     height: 400px;
+}
+
+.map-container {
+    position: relative;
+}
+
+.map-buttons {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    display: inline-block;
+    flex-direction: column;
 }
 
 .clear-button img {
